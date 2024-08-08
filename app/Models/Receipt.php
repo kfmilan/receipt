fb3d0f1d69c02c0d1c8309122e2b0124d9fecfd8
Kristoffer Milan <kfemilan@gmail.com>
2024-08-08 09:00:39 +0000

    initial commit

---
+++ app/Models/Receipt.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Receipt extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'date',
+        'name',
+        'service_charge_rate',
+        'user_id'
+    ];
+
+    protected $casts = [
+        'date' => 'date:Y-m-d',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2024_08_08_085036_create_receipts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('receipts', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('name');
+            $table->integer('service_charge_rate');
+            $table->foreignIdFor(User::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('receipts');
+    }
+};
