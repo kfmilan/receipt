f2b4c7716d9aa8ac528c267ca2123eba68364d9d
Kristoffer Milan <kfemilan@gmail.com>
2024-08-08 09:24:36 +0000

    feat: added Item model

--- app/Models/Receipt.php
+++ app/Models/Receipt.php
@@ -24,4 +24,9 @@ class Receipt extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
 }
---
+++ database/migrations/2024_08_08_091140_create_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Receipt;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Receipt::class);
+            $table->string('name');
+            $table->decimal('price, 10, 2');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
